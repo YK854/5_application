b551b3dab7b3804a9350ba80a22dfc9f796371dc
YK854 <95330893+YK854@users.noreply.github.com>
2021-12-16 10:10:35 +0000

    [Finish] 5_3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '12c9bcd42639591e1d6e48b899eb45be88f042e68c8e535d797baf477d4ffe5ed86976293f12e5f5c7ee77936b606b7622d1ef370ec5dddc6a840763ec5f60fa'
